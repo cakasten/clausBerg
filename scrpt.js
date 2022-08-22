24406258733a8902efe717708e4156b2bacd532f
Clint <40567645+cakasten@users.noreply.github.com>
2022-08-22 21:46:18 +0200

    fix fetch url

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,3 @@
 fetch(
-  "https://api.instagram.com/oauth/authorize?client_id=465215375479890&redirect_uri=file:///Users/clintkasten/Documents/Development/clausBerg/index.html&scope=user_profile,user_media&response_type=code"
+  "https://api.instagram.com/oauth/authorize?client_id=465215375479890&redirect_uri=https://cakasten.github.io/clausBerg/&scope=user_profile,user_media&response_type=code"
 ).then((res) => console.log(res.json()));
