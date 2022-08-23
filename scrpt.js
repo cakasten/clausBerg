92851030a69cc706da1b80a967387175e56a4b70
Clint <40567645+cakasten@users.noreply.github.com>
2022-08-23 23:11:59 +0200

    fix fetch

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,22 @@
+const appId = 3372859742957308;
+const redirectURI = "https://cakasten.github.io/clausBerg/";
+
 fetch(
-  "https://api.instagram.com/oauth/authorize?client_id=465215375479890&redirect_uri=https://cakasten.github.io/clausBerg/&scope=user_profile,user_media&response_type=code"
-).then((res) => console.log(res.json()));
+  `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redirectURI}&scope=user_profile,user_media&response_type=code`,
+  {mode: 'no-cors'}
+);
+
+
+//copy and paste into browser
+
+// https://api.instagram.com/oauth/authorize
+//   ?client_id=3372859742957308
+//   &redirect_uri=https://cakasten.github.io/clausBerg/
+//   &scope=user_profile,user_media
+//   &response_type=code
+
+
+
+
+
+
